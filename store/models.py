6914bcb1463a8f91a3f8731dcdcc509b917a0cd9
Namangoel781 <105874495+Namangoel781@users.noreply.github.com>
2023-09-09 03:34:32 +0530

    Add files via upload

--- store/models.py
+++ store/models.py
@@ -24,3 +24,16 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=110)
+    phone = models.CharField(max_length=110, default="")
+    address = models.CharField(max_length=110)
+    city = models.CharField(max_length=110)
+    state = models.CharField(max_length=110)
+    zip_code = models.CharField(max_length=110)
+    
--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from math import ceil
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 
 # Create your views here.
 def index(request):
@@ -49,4 +49,19 @@ def productView(request, myid):
     return render(request, 'store/prodView.html',{'product': product[0]})
 
 def checkout(request):
+    if request.method=="POST":
+        print(request)
+        items_json = request.POST.get('itemsJson', '')
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        phone=request.POST.get('phone', '')
+        address=request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city=request.POST.get('city', '')
+        state=request.POST.get('state', '')
+        zip_code=request.POST.get('zip_code', '')
+        order = Orders(items_json=items_json, name=name, email=email, phone=phone, address=address, city=city, state=state, zip_code=zip_code)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'store/checkout.html', {'thank': thank, 'id':id})
     return render(request, 'store/checkout.html')
